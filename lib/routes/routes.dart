c8cc5cb1f8e69d70e37ba9e3ab52ce5344bfdabf
Pedro <qsuma01@gmail.com>
2023-12-22 00:06:41 -0500

    Interfaz Chat sin ajustar logo y colores

---
+++ lib/routes/routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:taxis_app_public/pages/chat_page.dart';
+import 'package:taxis_app_public/pages/loading_page.dart';
+import 'package:taxis_app_public/pages/login_page.dart';
+import 'package:taxis_app_public/pages/register_page.dart';
+import 'package:taxis_app_public/pages/usuarios_page.dart';
+
+
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+
+  'usuarios': ( _ ) => UsuariosPage(),
+  'chat'    : ( _ ) => ChatPage(),
+  'login'   : ( _ ) => LoginPage(),
+  'register': ( _ ) => RegisterPage(),
+  'loading' : ( _ ) => LoadingPage(),
+
+};
+
+
